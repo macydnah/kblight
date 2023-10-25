43c570200e4c8135827192b0d6c790cdb51e022e
macydnah <juande.278@gmail.com>
2023-10-25 15:54:09 -0600

    better handling of kbd_backlight invalid states
    change s variable name to more descriptive check_state

--- kblight.c
+++ kblight.c
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])
 
 	/* Declarando variables y punteros de fichero. *
 	 * Inicializando variables.		       */
-	int state, automatic=1, on=1, off=1, s=1;
+	int state, automatic=1, on=1, off=1, check_state=1;
 	FILE *fpr, *fpw;
 
 	if(argc == 2) {
 	    automatic = strcmp("-auto", argv[1]);
 	    on = strcmp("-on", argv[1]);
 	    off = strcmp("-off", argv[1]);
-	    s = strcmp("-s", argv[1]); }
+	    check_state = strcmp("-s", argv[1]); }
 	else if(argc > 2) {
 	    printf("Error! too many arguments\n");
 	    exit(1); }
@@ -39,33 +39,25 @@ int main(int argc, char *argv[])
 	    exit(1); }
 
 	// Los meros meros.
-	if(state == -1) { fprintf(fpw, "%d\n", 0); }
 	if(argc == 1 || automatic == 0) {
-		if(state == 1) {
-		    fprintf(fpw, "%d\n", 0); }
+		if(state == 1) { fprintf(fpw, "%d\n", 0); }
+	   else if(state == -1 || state == 0 || state == 2) { fprintf(fpw, "%d\n", 1); }
+	}
 
-		else if(state == 0 || state == 2) {
-		    fprintf(fpw, "%d\n", 1); }
-	}
-	else if(on == 0) {
-		if(state == 0 || state == 1) {
-		    fprintf(fpw, "%d\n", 2); }
-	}
-	else if(off == 0) {
-		if(state == 1 || state == 2) {
-		    fprintf(fpw, "%d\n", 0); }
-	}
-	else if(s == 0) {
-		if(state == -1) { printf("Actual state is: Invalid State!\n");  }
+	else if(on == 0)  { fprintf(fpw, "%d\n", 2); }
+	else if(off == 0) { fprintf(fpw, "%d\n", 0); }
+
+	else if(check_state == 0) {
 		if(state == 0) { printf("Actual state is: off\n");  }
 		if(state == 1) { printf("Actual state is: auto\n"); }
 		if(state == 2) { printf("Actual state is: on\n");   }
 	}
+
 	else { printf("Error! invalid argument\n"); }
 
 	// Cerrando ficheros.
-	fclose(fpr);
 	fclose(fpw);
+	fclose(fpr);
 
 	return 0;
 }
