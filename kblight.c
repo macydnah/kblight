6194c8dc9a7aaaa66d67f1b7e313063e768b1559
macydnah <juande.278@gmail.com>
2023-11-02 23:33:13 -0600

    refactor(kblight.c): total rewrite of codebase
    Simplify the codebase a lot. Breaking it down main() to simpler
    functions, making the whole codebase easier to main()tain.
    
    - Break down of main() results in the writing(), toggle() and estado()
      functions added
    
    - writing() function now manages writing operations on FILE pointer at
      convenience. No more losing of state variable value by invoking
      fopen() in write mode out of context
    
    - Change string.h for unistd.h. Getting rid of string comparison
      mechanism for parsing command line options. Use getopt() function
      and a while-switch-case flow control instead
    
    - Better handling of FILE pointer. Use just one variable to handle
      fopen() read and write operations
    
    - Make all variables static. Cleanup variables. Reduce it's number to
      just three. Change "unsigned short int" type ones to "short int" type

--- kblight.c
+++ kblight.c
@@ -1,53 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include <unistd.h>
 #define SYSPATH "/sys/devices/platform/sony-laptop/kbd_backlight"
+
+static short int opt, state;
+static FILE *fp;
+
+void writing() { if((fp = fopen(SYSPATH,"w")) == NULL) { printf("Error! writing file\n"); exit(1); } }
+
+void toggle() {
+	if(state == 1) { fprintf(fp, "%hd\n", 0); }
+	else if(state == -1 || state == 0 || state == 2) { fprintf(fp, "%hd\n", 1); }
+	fclose(fp); }
+
+void estado() {
+	if(state == 0) { printf("Actual state is: off\n"); }
+	else if(state == 1) { printf("Actual state is: auto\n"); }
+	else if(state == 2) { printf("Actual state is: on\n"); }
+	exit(EXIT_SUCCESS); }
 
 int main(int argc, char *argv[])
 {
+	if(argc > 2) { printf("Error! too many arguments\n"); exit(1); }
 
-	// Declarando e inicializando variables.
-	unsigned short int state, automatic=1, on=1, off=1, check_state=1;
-	FILE *fp;
-
-	if(argc == 2) {
-	    automatic = strcmp("-auto", argv[1]);
-	    on = strcmp("-on", argv[1]);
-	    off = strcmp("-off", argv[1]);
-	    check_state = strcmp("-s", argv[1]); }
-	else if(argc > 2) { printf("Error! too many arguments\n"); exit(1); }
-
-	// Declarando e inicializando puntero de fichero.
 	if((fp = fopen(SYSPATH,"r")) == NULL) { printf("Error! reading file\n"); exit(1); }
+	fscanf(fp, "%hd", &state); fclose(fp);
 
-	// Inicializando variable state con valor de estado.
-	fscanf(fp, "%hu", &state); fclose(fp);
+	while ((opt = getopt(argc, argv, "aofs")) != -1) {
+	    switch (opt) {
+	    case 'a': writing(); toggle(); break;
+	    case 'o': writing(); fprintf(fp, "%hd\n", 2); break;
+	    case 'f': writing(); fprintf(fp, "%hd\n", 0); break;
+	    case 's': estado(); } }
 
-	// Revisando estado.
-	if(check_state == 0) {
-		if(state == 0) { printf("Actual state is: off\n");  }
-		if(state == 1) { printf("Actual state is: auto\n"); }
-		if(state == 2) { printf("Actual state is: on\n");   }
-		exit(EXIT_SUCCESS);
-	}
-
-	// Inicializando puntero en modo escritura.
-	if((fp = fopen(SYSPATH,"w")) == NULL) { printf("Error! writing file\n"); exit(1); }
-
-	// Los meros meros.
-	if(argc == 1 || automatic == 0) {
-		if(state == 1) { fprintf(fp, "%hu\n", 0); }
-	   else if(state == -1 || state == 0 || state == 2) { fprintf(fp, "%hu\n", 1); }
-	}
-
-	else if(on == 0)  { fprintf(fp, "%hu\n", 2); }
-	else if(off == 0) { fprintf(fp, "%hu\n", 0); }
-
-	else { printf("Error! invalid argument\n"); }
-
-	// Cerrando fichero.
-	fclose(fp);
-
-	return 0;
+	writing(); toggle(); return 0;
 }
