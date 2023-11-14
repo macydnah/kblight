90933ca349ddcf376c53cb6ec4abbc0f82b4755c
macydnah <juande.278@gmail.com>
2023-11-14 13:50:34 -0600

    refactor(kblight.c): handle error messages to stderr
    Better to practice good coding practices 🤷. Use fprintf() instead of
    printf() to print error messages to stderr.
    
    - Rename "state" variable to the more descriptive one "mode"
    
    - Simplify toggle() function

--- kblight.c
+++ kblight.c
@@ -3,28 +3,28 @@
 #include <unistd.h>
 #define SYSPATH "/sys/devices/platform/sony-laptop/kbd_backlight"
 
-static short int opt, state;
+static short int opt, mode;
 static FILE *fp;
 
-void writing() { if((fp = fopen(SYSPATH,"w")) == NULL) { printf("Error! writing file\n"); exit(1); } }
+void writing() { if((fp = fopen(SYSPATH,"w")) == NULL) { fprintf(stderr, "Error! writing file.\n"); exit(1); } }
 
 void toggle() {
-	if(state == 1) { fprintf(fp, "%hd\n", 0); }
-	else if(state == -1 || state == 0 || state == 2) { fprintf(fp, "%hd\n", 1); }
+	if(mode == 1) { fprintf(fp, "%hd\n", 0); }
+	else { fprintf(fp, "%hd\n", 1); }
 	fclose(fp); }
 
 void estado() {
-	if(state == 0) { printf("Actual state is: off\n"); }
-	else if(state == 1) { printf("Actual state is: auto\n"); }
-	else if(state == 2) { printf("Actual state is: on\n"); }
+	if(mode == 0) { printf("Actual active mode is: off\n"); }
+	else if(mode == 1) { printf("Actual active mode is: auto\n"); }
+	else if(mode == 2) { printf("Actual active mode is: on\n"); }
 	exit(EXIT_SUCCESS); }
 
 int main(int argc, char *argv[])
 {
-	if(argc > 2) { printf("Error! too many arguments\n"); exit(1); }
+	if(argc > 2) { fprintf(stderr, "Error! too many arguments.\n"); exit(1); }
 
-	if((fp = fopen(SYSPATH,"r")) == NULL) { printf("Error! reading file\n"); exit(1); }
-	fscanf(fp, "%hd", &state); fclose(fp);
+	if((fp = fopen(SYSPATH,"r")) == NULL) { fprintf(stderr, "Error! reading file.\n"); exit(1); }
+	fscanf(fp, "%hd", &mode); fclose(fp);
 
 	while ((opt = getopt(argc, argv, "aofs")) != -1) {
 	    switch (opt) {
